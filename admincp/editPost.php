065a3574a2fa90de5313d39e74cfeb14b06cb8c4
trungtuan97 <trungtuanlinh9799@gmail.com>
2023-11-17 23:32:27 +0700

    fix styles

--- admincp/editPost.php
+++ admincp/editPost.php
@@ -20,8 +20,10 @@ if (isset($_GET['edit'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/styleadmin.css">
-    <title>admin</title>
+    <title>edit</title>
 </head>
+
+
 
 <body>
     <div class="editPost">
@@ -32,16 +34,13 @@ if (isset($_GET['edit'])) {
         <form class="from" action='' method='POST'>
 
             <label for='title'>Tên bài viết:</label>
-            <input class="title" type='text' id='title' name='title'
-                value="<?php echo ($edit && $post) ? $post['title'] : ''; ?>">
+            <input class="title" type='text' id='title' name='title' value="<?php echo ($edit && $post) ? $post['title'] : ''; ?>">
             <br>
             <label for='image'>Link ảnh:</label>
-            <input class="image" type='text' id='image' name='image'
-                value="<?php echo ($edit && $post) ? $post['img'] : ''; ?>">
+            <input class="image" type='text' id='image' name='image' value="<?php echo ($edit && $post) ? $post['img'] : ''; ?>">
             <br>
             <label for='content'>Nội dung bài viết:</label>
-            <textarea class="content" id='content'
-                name='content'><?php echo ($edit && $post) ? $post['content'] : ''; ?></textarea>
+            <textarea class="content" id='content' name='content'><?php echo ($edit && $post) ? $post['content'] : ''; ?></textarea>
             <br>
             <button class="btl" type='submit'>
                 <?php echo $status ?>
